3e0ae33e06a4f6630516ff6e8462bc2a796b6c36
DungNT <tridungtin8@gmail.com>
2024-05-30 23:24:23 +0700

    update

--- src/main/java/com/microservice/user/utils/GoogleUtils.java
+++ src/main/java/com/microservice/user/utils/GoogleUtils.java
@@ -14,23 +14,9 @@ import java.util.Collections;
 
 @Component
 public class GoogleUtils {
-    @Value("${spring.security.oauth2.client.registration.google.clientId}")
     private String CLIENT_ID;
     public UserInfoResponseDto getUserInfo(String idTokenString) throws GeneralSecurityException, IOException {
-        GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(new NetHttpTransport(), new GsonFactory())
-                .setAudience(Collections.singletonList(CLIENT_ID))
-                .build();
-        GoogleIdToken idToken = verifier.verify(idTokenString);
-        if (idToken != null) {
 
-            GoogleIdToken.Payload payload = idToken.getPayload();
-            return UserInfoResponseDto.builder()
-                    .username(payload.getEmail().split("@")[0].trim())
-                    .email(payload.getEmail())
-                    .name((String) payload.get("name"))
-                    .image((String) payload.get("picture"))
-                    .build();
-        }
         return null;
     }
 }
